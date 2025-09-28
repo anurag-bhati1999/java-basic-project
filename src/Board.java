3dc104ec6792606dcac82de6eebef69f8164cd7d
anurag-bhati1999 <anurag.bhati1999@gmail.com>
2025-09-28 17:03:20 +0530

    complete game service

--- src/Board.java
+++ src/Board.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class Board{
     private ArrayList<Snake>snakes;
     private  ArrayList<Ladder>ladders;
+    private final int size = 100;
 
     public ArrayList<Snake>getSnakes(){
         return snakes;
@@ -11,6 +12,10 @@ public class Board{
 
     public ArrayList<Ladder>getLadders(){
         return ladders;
+    }
+
+    public int getSize(){
+        return size;
     }
 
     public void setSnakes(ArrayList<Snake>snakes){
--- src/GameService.java
+++ src/GameService.java
@@ -1,45 +1,84 @@
 
 import java.util.*;
 
-public class GameService{
-    private Board board;
-    private List<Player>players;
-    private Map<String,Integer>positions;
+public class GameService {
 
-    public GameService(Board board, List<Player>players){
+    private Board board;
+    private List<Player> players;
+    private Map<String, Integer> positions;
+
+    public GameService(Board board, List<Player> players) {
         this.board = board;
         this.players = players;
     }
 
-    public List<Player> getPlayers(){
-        return  players;
+    public List<Player> getPlayers() {
+        return players;
     }
 
-    public Board getBoard(){
+    public Board getBoard() {
         return board;
     }
 
-    public Map<String, Integer> getPlayerPositions(){
+    public Map<String, Integer> getPlayerPositions() {
         return positions;
     }
 
-    public int getPositionOfPlayer(Player player){
-        String id  = player.getId();
+    public int getPositionOfPlayer(Player player) {
+        String id = player.getId();
         return positions.get(id);
     }
 
-    public int getNewPosition(int oldPosition, int score ){
-
+    public void setPositionOfPlayer(Player player, int position) {
+        String id = player.getId();
+        positions.put(id, position);
     }
 
-    public void playGame(){
-        for(Player player : players){
-            int score = DiceService.rollDice();
-            int oldPosition = this.getPositionOfPlayer(player);
-            int newPosition = getNewPosition(oldPosition, score);
+    public int getNewPosition(int oldPosition, int score) {
+        if (oldPosition + score > board.getSize()) {
+            return oldPosition;
+        }
+
+        int newPosition = oldPosition + score;
+        boolean noSnakeAndLadder = true;
+        do {
+            for (Snake snake : board.getSnakes()) {
+                if (newPosition == snake.getStart()) {
+                    newPosition = snake.getEnd();
+                    noSnakeAndLadder = false;
+                    break;
+                }
+            }
+
+            for (Ladder ladder : board.getLadders()) {
+                if (newPosition == ladder.getEnd()) {
+                    newPosition = ladder.getEnd();
+                    noSnakeAndLadder = false;
+                }
+            }
+        } while (noSnakeAndLadder);
+
+        return newPosition;
+    }
+
+    public void playGame() {
+        boolean won = false;
+        while (!won) {
+            for (Player player : players) {
+                int score = DiceService.rollDice();
+                int oldPosition = this.getPositionOfPlayer(player);
+                int newPosition = getNewPosition(oldPosition, score);
+                setPositionOfPlayer(player, newPosition);
+                System.out.println("Player " + player.getName()
+                        + " score " + score + " position " + newPosition);
+
+                if (newPosition == board.getSize()) {
+                    System.err.println("Player " + player.getName() + " won.");
+                    won = true;
+                    break;
+                }
+            }
         }
     }
-
-
 
 }
