d9b158515d248d66189f4b6cec3c4b6493ee7f75
anurag-bhati1999 <anurag.bhati1999@gmail.com>
2025-09-30 15:14:59 +0530

    main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String args[]) {
+        Player player1 = new Player.PlayerBuilder()
+                .withName("Anurag")
+                .withStrategy(new HumanPlayerStrategy())
+                .withSymbol(Symbol.X)
+                .build();
+
+        Player player2 = new Player.PlayerBuilder()
+                .withName("Yash")
+                .withStrategy(new HumanPlayerStrategy())
+                .withSymbol(Symbol.O)
+                .build();
+
+        List<Player> players = new ArrayList<>(List.of(player1, player2));
+        Game game = new Game(players);
+        game.play();
     }
 }
