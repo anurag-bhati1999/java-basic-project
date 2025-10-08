c0e90258d37caf81cc0ab3057478c06fa0e6b510
anurag-bhati1999 <anurag.bhati1999@gmail.com>
2025-10-08 13:22:45 +0530

    play function

--- src/Board.java
+++ src/Board.java
@@ -1,18 +1,28 @@
-public class Board{
+
+public class Board {
+
     private int width;
     private int height;
     private static Board instance;
 
-    private Board(int width, int height){
+    private Board(int width, int height) {
         this.width = width;
         this.height = height;
     }
 
-    public static Board getInstance(int  width, int height){
-        if(instance == null){
+    public static Board getInstance(int width, int height) {
+        if (instance == null) {
             return new Board(width, height);
         }
 
         return instance;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
     }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,68 @@
-public class Game{
+
+public class Game {
+
     private Snake snake;
     private Board board;
     private int food[][];
+    private PlayerStrategy playerStrategy;
+    private int foodIndex;
+    private int currScore;
 
-    public Game(int width, int height, int food[][]){
+    public Game(int width, int height, int food[][], PlayerStrategy playerStrategy) {
         this.snake = new Snake();
         this.board = Board.getInstance(width, height);
         this.food = food;
+        this.playerStrategy = playerStrategy;
+        this.foodIndex = 0;
+        this.currScore = 0;
     }
+
+    public void play() {
+        boolean isGameRunning = true;
+
+        int height = board.getHeight();
+        int width = board.getWidth();
+
+        while (isGameRunning) {
+            Position head = snake.body.peekFirst();
+            Position tail = snake.body.peekLast();
+            Position next = playerStrategy.getNextPosition(head);
+
+            int row = next.row;
+            int col = next.col;
+
+            if (row < 0 || col < 0 || row > height || col > width) {
+                System.out.println("You crossed boundary");
+                isGameRunning = false;
+                continue;
+            }
+
+            if (snake.positions.containsKey(next)) {
+                isGameRunning = false;
+                System.out.println("You ate yourself");
+                continue;
+            }
+
+            int nextRow = next.row;
+            int nextCol = next.col;
+
+            boolean ateFood = foodIndex < food.length
+                    && food[foodIndex][0] == nextRow
+                    && food[foodIndex][1] == nextCol;
+
+            if (ateFood) {
+                foodIndex++;
+            } else {
+                // remove taile
+                this.snake.body.removeLast();
+                this.snake.positions.remove(tail);
+            }
+
+            this.currScore = this.snake.body.size() - 1;
+            this.snake.body.addFirst(next);
+            this.snake.positions.put(next, true);
+
+        }
+    }
+
 }
