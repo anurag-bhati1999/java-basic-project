3e4ced3cacfe8631dc51a76add5b6cc8df3159a6
anurag-bhati1999 <anurag.bhati1999@gmail.com>
2025-10-08 12:41:44 +0530

    fix

--- src/HumanPlayerStrategy.java
+++ src/HumanPlayerStrategy.java
@@ -1,26 +1,29 @@
+
+import java.util.*;
+
 public class HumanPlayerStrategy implements PlayerStrategy {
-    public Position getNexPosition(Position curr){
-      
-       int i=0;
-       for(i=0;i<3;i++){
-         Scanner scanner = new Scanner(system.in);
-         String pos = scanner.next();
-            switch(pos){
-            case 'U':
-                return new Position(curr.row+1, curr.col);
-            case 'D':
-                return new Position(curr.row -1, curr.col);
-            case 'L':
-                return new Position(curr.row, curr.col-1);
-            case 'R':
-                return new Position(curr.row, curr.col+1);
-            default:
-                continue;
-       }
-       }
 
-       return null;
+    public Position getNextPosition(Position curr) {
 
-       
+        int i = 0;
+        for (i = 0; i < 3; i++) {
+            Scanner scanner = new Scanner(System.in);
+            String pos = scanner.next();
+            switch (pos) {
+                case "U":
+                    return new Position(curr.row + 1, curr.col);
+                case "D":
+                    return new Position(curr.row - 1, curr.col);
+                case "L":
+                    return new Position(curr.row, curr.col - 1);
+                case "R":
+                    return new Position(curr.row, curr.col + 1);
+                default:
+                    continue;
+            }
+        }
+
+        return null;
+
     }
 }
