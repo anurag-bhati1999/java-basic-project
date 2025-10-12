d879a7f6a51d746c9df406bace80c2d6ea5dbdc5
anurag-bhati1999 <anurag.bhati1999@gmail.com>
2025-10-12 10:38:19 +0530

    product

---
+++ src/Product.java
@@ -0,0 +1,65 @@
+
+public class Product {
+
+    private int id;
+    private String name;
+    private ProductCategory category;
+    private int quantity;
+    private int price;
+    private int threshold;
+
+    private Product(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.category = builder.category;
+        this.quantity = builder.quantity;
+        this.price = builder.price;
+        this.threshold = builder.threshold;
+    }
+
+    public static class Builder {
+
+        private int id;
+        private String name;
+        private ProductCategory category;
+        private int quantity;
+        private int price;
+        private int threshold;
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setQuantity(int quantity) {
+            this.quantity = quantity;
+            return this;
+        }
+
+        public Builder setCategory(ProductCategory category){
+            this.category = category;
+            return this;
+        }
+
+        public Builder setPrice(int price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder setThreshold(int threshold) {
+            this.threshold = threshold;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
+
+    }
+
+}
---
+++ src/ProductCategory.java
@@ -0,0 +1,3 @@
+public enum ProductCategory{
+    CLOTH, ELECTRIC, FURNITURE
+}
